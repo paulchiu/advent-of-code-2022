a58a39020e633b69ebfc0e2474ba466c50d4e081
Paul Chiu <paulchiu@gmail.com>
2022-12-04 13:59:00 +1000

    day(1): Made elf reader add to array

--- day1/solution.py
+++ day1/solution.py
@@ -2,21 +2,30 @@
 elf = 1
 printed_up_to_elf_number = 0
 
+# Dictionary of elf numbers and their calories of food carried
+elf_calories = {}
+
 # Read input file
 with open('input.txt', 'r') as f:
     # Read all lines in file
     while line := f.readline():
-        # If elf number is not printed yet
-        if elf > printed_up_to_elf_number:
-            # Print elf number
-            print(f"Elf number: {elf}")
-            printed_up_to_elf_number += 1
+        # If elf number is not in dictionary
+        if elf not in elf_calories:
+            # Add elf number to dictionary
+            elf_calories[elf] = []
 
-        print(line)
 
+        # If line is a new line, increment elf number
+        # otherwise add calories to elf number
         if line == '\n':
             elf = elf + 1
+        else:
+            elf_calories[elf].append(int(line))
 
         # Stop script after elf 3
         if elf == 3:
             break
+
+# Print elf number and calories of food carried
+for elf in elf_calories:
+    print(f'Elf {elf} has {elf_calories[elf]} calories of food')
