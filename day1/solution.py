176956b06e50cd06cce4ca878ba79320bcef0331
Paul Chiu <paulchiu@gmail.com>
2022-12-04 14:19:18 +1000

    day(1): Refactored solution into functions

--- day1/solution.py
+++ day1/solution.py
@@ -1,36 +1,42 @@
-# Track elf number
-elf = 1
-printed_up_to_elf_number = 0
+# Elf file reader function
+def elf_reader(filename):
+    # Track elf number
+    elf = 1
+    printed_up_to_elf_number = 0
 
-# Dictionary of elf numbers and their calories of food carried
-elf_calories = {}
+    # Dictionary of elf numbers and their calories of food carried
+    elf_calories = {}
 
-# Read input file
-with open('input.txt', 'r') as f:
-    # Read all lines in file
-    while line := f.readline():
-        # If elf number is not in dictionary
-        if elf not in elf_calories:
-            # Add elf number to dictionary
-            elf_calories[elf] = []
+    # Read input file
+    with open(filename, 'r') as f:
+        # Read all lines in file
+        while line := f.readline():
+            # If elf number is not in dictionary
+            if elf not in elf_calories:
+                # Add elf number to dictionary
+                elf_calories[elf] = []
 
-        # If line is a new line, increment elf number
-        # otherwise add calories to elf number
-        if line == '\n':
-            elf = elf + 1
-        else:
-            elf_calories[elf].append(int(line))
+            # If line is a new line, increment elf number
+            # otherwise add calories to elf number
+            if line == '\n':
+                elf = elf + 1
+            else:
+                elf_calories[elf].append(int(line))
 
-# Add all calories of food carried by each elf
-elf_total_calories = { elf: sum(calories) for elf, calories in elf_calories.items() }
+    return elf_calories
 
-# Sort elf total calories by value
-elf_total_calories_sorted = { elf: calories for elf, calories in sorted(elf_total_calories.items(), key=lambda item: item[1], reverse=True) }
+def main():
+    elf_calories = elf_reader('input.txt')
+    elf_total_calories = { elf: sum(calories) for elf, calories in elf_calories.items() }
+    elf_total_calories_sorted = sorted(elf_total_calories.items(), key=lambda item: item[1], reverse=True)
 
-# Print all elf numbers and their total calories of food carried
-for elf, calories in elf_total_calories_sorted.items():
-    print(f'Elf {elf} has a total of {calories} calories')
+    # Print all elf numbers and their total calories of food carried
+    for elf, calories in elf_total_calories_sorted:
+        print(f'Elf {elf} carried {calories} calories of food')    
 
-# Print the elf with the most calories of food carried
-print('\n')
-print(f'Elf {list(elf_total_calories_sorted.keys())[0]} has the most calories of food carried with {list(elf_total_calories_sorted.values())[0]} calories')
+    # Print the elf with the most calories of food carried
+    print('\n')
+    print(f'Elf {elf_total_calories_sorted[0][0]} carried the most food with {elf_total_calories_sorted[0][1]} calories')
+
+if __name__ == '__main__':
+    main()
